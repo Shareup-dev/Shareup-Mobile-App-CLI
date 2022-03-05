cdd2b41d282f47c3933289d9b20c97f8ef95e941
Lokeesan <logeeshan1@gmail.com>
2022-03-05 15:45:28 +0300

    moving all files

--- src/components/Icons/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Icon from 'react-native-vector-icons/Ionicons';
-
-export default function Icons(props) {
-  const {size = 22, ...rest} = props;
-  return <Icon {...rest} size={size} />;
-}
