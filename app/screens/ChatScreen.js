6e6c3d1e819b57cfeaa80eb3910f3ca44914e94d
Lokeesan <logeeshan1@gmail.com>
2022-04-24 14:52:00 +0300

    Update ChatScreen.js

--- app/screens/ChatScreen.js
+++ app/screens/ChatScreen.js
@@ -5,6 +5,8 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
+  Dimensions,
+  ScrollView,
 } from 'react-native';
 import {HeaderWithBackArrow} from '../components/headers';
 import Icon from '../components/Icon';
@@ -13,74 +15,102 @@ import colors from '../config/colors';
 export default function ChatScreen({navigation}) {
   const [message, setMessage] = useState('');
 
-  const MessageCard = ({left}) => {
+  const MessageCard = ({right}) => {
     return (
       <View
         style={{
-          backgroundColor: '#fff',
-          margin: 5,
-          paddingHorizontal: 15,
-          paddingVertical: 10,
-          borderRadius: 10,
-          maxWidth: '70%',
-          alignSelf: left ? 'flex-end' :'flex-start'
+          alignSelf: right ? 'flex-end' : 'flex-start',
         }}>
+        <View
+          style={{
+            backgroundColor: '#fff',
+            margin: 5,
+            paddingHorizontal: 15,
+            paddingVertical: 10,
+            borderRadius: 10,
+            maxWidth: '70%',
+          }}>
+          <Text
+            style={{
+              fontWeight: '800',
+              fontSize: 14,
+            }}>
+            Kanesha Lokeesan
+          </Text>
+          <Text
+            style={{
+              marginVertical: 3,
+              fontSize: 14,
+            }}>
+            document or a typeface without relying on meaningful content.
+          </Text>
+        </View>
         <Text
           style={{
-            fontWeight: '800',
-            fontSize: 14,
+            textAlign: right ? 'right' : 'left',
+            marginHorizontal: 15,
+            fontSize: 12,
           }}>
-          Kanesha Lokeesan
-        </Text>
-        <Text
-          style={{
-            marginVertical: 3,
-            fontSize: 14,
-          }}>
-         document or a typeface without relying on meaningful content.
+          12:00
         </Text>
       </View>
     );
   };
-
+  const {width} = Dimensions.get('window');
   return (
     <View style={styles.container}>
-      <HeaderWithBackArrow title={'Kaneshamoorthi Lokeesan'} titleStyle={{fontSize:16}} onBackButton={_ => navigation.goBack()} />
+      <HeaderWithBackArrow
+        title={'Kaneshamoorthi Lokeesan'}
+        titleStyle={{fontSize: 16}}
+        onBackButton={_ => navigation.goBack()}
+      />
+      <ScrollView
+        style={{
+          paddingHorizontal: 10,
+          flex: 1,
+          marginBottom: 90,
+        }}
+        >
+        <>
+          <MessageCard />
+          <MessageCard />
+          <MessageCard />
+          <MessageCard right />
+          <MessageCard right />
+          <MessageCard />
+          <MessageCard />
+          <MessageCard right />
+          <MessageCard right />
+        </>
+      </ScrollView>
+
       <View
         style={{
-          marginHorizontal: 10,
-          justifyContent:'flex-start'
-
-        }}>
-        <MessageCard />
-        <MessageCard left />
-        <MessageCard />
-        </View>
-
-        <View
-          style={{
-            position: 'absolute',
-            bottom: 25,
-            flexDirection: 'row',
-            alignItems: 'center',
-          marginHorizontal: 10,
+          position: 'absolute',
+          bottom: 0,
+          flexDirection: 'row',
+          alignItems: 'center',
+          paddingHorizontal: 10,
+          paddingVertical: 15,
           justifyContent: 'space-between',
+          width: width,
+          backgroundColor: '#fff',
+        }}>
+        <TextInput style={styles.message} placeholder="message" />
+        <TouchableOpacity
+          style={{
+            backgroundColor: colors.iondigoDye,
+            padding: 5,
+            borderRadius: 25,
           }}>
-          <TextInput style={styles.message} placeholder="message" />
-          <TouchableOpacity
-            style={{
-              backgroundColor: colors.iondigoDye,
-              padding: 5,
-              borderRadius: 25,
-            }}>
-            <Icon
-              noBackground
-              name={'send'}
-              type="FontAwesome"
-              color="#fff"
-              backgroundSizeRatio={0.7}
-            />
-          </TouchableOpacity>
+          <Icon
+            noBackground
+            name={'send'}
+            type="FontAwesome"
+            color="#fff"
+            backgroundSizeRatio={0.7}
+          />
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -89,6 +119,7 @@ export default function ChatScreen({navigation}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#cacaca60',
   },
   forwardArrow: {
     position: 'absolute',
