eb129a191305aa91f28fa754060ce43d82b1ef88
Lokeesan <logeeshan1@gmail.com>
2022-05-15 15:00:51 +0300

    Update CustomImageSlider.js

--- app/components/ImageSlider/CustomImageSlider.js
+++ app/components/ImageSlider/CustomImageSlider.js
@@ -1,38 +1,52 @@
-import React, { memo, useRef, useState } from 'react';
-import {View,  Dimensions,FlatList, TouchableOpacity} from 'react-native';
+import React, {memo, useRef, useState} from 'react';
+import {
+  View,
+  Dimensions,
+  FlatList,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 import BetterImage from '../betterImage/BetterImage';
 
+function CustomImageSlider({media, width, height}) {
+  const [activeIndex, setActiveIndex] = useState(0);
 
-function   CustomImageSlider({media,width,height}) {
-    const [activeIndex, setActiveIndex] = useState(0);
-  
-    const onViewRef = useRef(({viewableItems}) => {
-      setActiveIndex(viewableItems[0].index);
-    });
-    const viewConfigRef = useRef({viewAreaCoveragePercentThreshold: 50});
+  const onViewRef = useRef(({viewableItems}) => {
+    setActiveIndex(viewableItems[0].index);
+  });
+  const viewConfigRef = useRef({viewAreaCoveragePercentThreshold: 50});
   const flatListRef = useRef();
 
   return (
     <>
-      <FlatList
+      {/* <FlatList
         onViewableItemsChanged={onViewRef.current}
         viewabilityConfig={viewConfigRef.current}
         horizontal
+        pagingEnabled 
         ref={flatListRef}
-        pagingEnabled
-        showsHorizontalScrollIndicator={false}
+       
         data={media}
         keyExtractor={({item}, index) => index.toString()}
         renderItem={({item}) => {
           return (
-            <BetterImage
-              style={{width, height}}
-              resizeMode={'cover'}
-              source={{uri: item.mediaPath}}
-            />
+          
           );
         }}
-      />
+      /> */}
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}>
+        {media.map((image, index) => (
+          <BetterImage
+            key={index}
+            style={{width, height}}
+            resizeMode={'cover'}
+            source={{uri: image.mediaPath}}
+          />
+        ))}
+      </ScrollView>
       <View>
         <View
           style={{
@@ -41,7 +55,7 @@ function   CustomImageSlider({media,width,height}) {
             alignSelf: 'center',
           }}>
           {media?.length > 1 &&
-           media.map(({media}, index) => (
+            media.map(({media}, index) => (
               <TouchableOpacity
                 key={index}
                 style={{
@@ -66,4 +80,3 @@ function   CustomImageSlider({media,width,height}) {
 }
 
 export default React.memo(CustomImageSlider);
-
